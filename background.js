6ab620ebc69ced174e6894cb9396c0b966a1114f
Josh Levy <joshlevy89@gmail.com>
2016-05-21 07:00:06 -0400

    add background page to toggle status of extension

---
+++ background.js
@@ -0,0 +1,14 @@
+var toggle = false;
+console.log('hello');
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggle = !toggle;
+  console.log(toggle);
+  if(toggle){
+    //chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
+    chrome.tabs.executeScript(null, {file:"openTooltipMenu.js"});
+  }
+  else{
+    //chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"temp.js"});
+  }
+});
