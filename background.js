ea47ef6b011dc74e2051cb873bca288eaf743689
Josh Levy <joshlevy89@gmail.com>
2016-05-23 17:13:11 -0400

    enabled toggling of on/off state from popup.js rather than as a browser action; this will allow for many more items and actions from the popup menu

--- background.js
+++ background.js
@@ -1,17 +1,12 @@
-// turn content script on by default
-var toggle = true;
-chrome.tabs.executeScript({file:"openTooltipMenu.js"}); 
-console.log('hello');
+var isExtensionOn = true;
 
-// toggle event listener each time extension icon clicked
-chrome.browserAction.onClicked.addListener(function(tab) {
-  toggle = !toggle;
-  if(toggle){
-    chrome.browserAction.setIcon({path: "./icons/icon_on.png"});
-    chrome.tabs.executeScript({file:"openTooltipMenu.js"});
-  }
-  else{
-    chrome.browserAction.setIcon({path: "./icons/icon_off.png"});
-    chrome.tabs.executeScript({code: "$('body').off();"});
-  }
+chrome.extension.onMessage.addListener(
+function (request, sender, sendResponse) {
+    if (request.cmd == "setOnOffState") {
+        isExtensionOn = request.data.value;
+    }
+
+    if (request.cmd == "getOnOffState") {
+        sendResponse(isExtensionOn);
+    }
 });
