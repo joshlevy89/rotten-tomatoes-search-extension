d5cd772e0a96400d90cd18ce28209901c4fc2bb4
Josh Levy <joshlevy89@gmail.com>
2016-05-23 17:14:22 -0400

    got rid of a console.log statement

--- popup.js
+++ popup.js
@@ -3,13 +3,12 @@ var isExtensionOn = false;
 
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('toggle').addEventListener('click', function() {
-		chrome.tabs.executeScript({code: "console.log('toggled...')"});
+		//chrome.tabs.executeScript({code: "console.log('toggled...')"});
 		chrome.extension.sendMessage({ cmd: "getOnOffState" }, function(currentState){
 			var newState = !currentState;
 			// toggle to the new state in background
 			chrome.extension.sendMessage({ cmd: "setOnOffState", data: { value: newState } }, function(){
 				// after toggling, do stuff based on new state
-				chrome.tabs.executeScript({code: "console.log( " + newState + ");"});
 				if (newState) {
 					chrome.browserAction.setIcon({path: "./icons/icon_on.png"});
 					chrome.tabs.executeScript({file:"openTooltipMenu.js"});
