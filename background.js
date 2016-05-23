e18e7c1c58b575ab59316610812bd89a600147e9
Josh Levy <joshlevy89@gmail.com>
2016-05-23 15:23:42 -0400

    added custom icons for on and off states and allowed background page to setIcon

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 // turn content script on by default
 var toggle = true;
-chrome.tabs.executeScript(null, {file:"openTooltipMenu.js"}); 
+chrome.tabs.executeScript({file:"openTooltipMenu.js"}); 
 console.log('hello');
 
 // toggle event listener each time extension icon clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
   toggle = !toggle;
   if(toggle){
-    //chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
-    chrome.tabs.executeScript(null, {file:"openTooltipMenu.js"});
+    chrome.browserAction.setIcon({path: "./icons/icon_on.png"});
+    chrome.tabs.executeScript({file:"openTooltipMenu.js"});
   }
   else{
-    //chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, {code: "$('body').off();"});
+    chrome.browserAction.setIcon({path: "./icons/icon_off.png"});
+    chrome.tabs.executeScript({code: "$('body').off();"});
   }
 });
